65282a4120bd5dd219cf97e73ce8a4b8ea60b413
Chap Carr <78283294+ChapCarr@users.noreply.github.com>
2025-03-27 18:10:08 -0500

    added zines

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const hamburgerEl = document.querySelector('.hamburger');
+/*const hamburgerEl = document.querySelector('.hamburger');
 const navEl = document.querySelector('.nav');
 
 
@@ -6,4 +6,21 @@ hamburgerEl.addEventListener('click', () =>{
     navEl.classList.toggle('nav-open');
     hamburgerEl.classList.toggle('hamburger-open');
 } )
+*/
 
+
+// Load nav.html first
+fetch('nav.html')
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById('navbar').innerHTML = data;
+
+    // Now the nav is in the page, so we can safely select it:
+    const hamburgerEl = document.querySelector('.hamburger');
+    const navEl = document.querySelector('.nav');
+
+    hamburgerEl.addEventListener('click', () => {
+      navEl.classList.toggle('nav-open');
+      hamburgerEl.classList.toggle('hamburger-open');
+    });
+  });
